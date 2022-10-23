6b9b885cf88898be37941b598c480b349a3eb6a7
gabbarX <ankit.rr528@gmail.com>
2022-10-23 16:08:26 +0530

    created empty external command files

---
+++ Assignment 1/ls.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <pwd.h>
+#include <time.h>
+#include <grp.h>
+
+int main(argc, char *argv[])
+{
+    char command[10];
+    char flag[10];
+    char argument[10];
+}
