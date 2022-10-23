c6a7ba5a8010621195ef20083fe6a3e5d461daa5
Ankit Gautam <ankit.rr528@gmail.com>
2022-10-23 13:36:09 +0530

    echo implemented

--- Assignment 1/shell.c
+++ Assignment 1/shell.c
@@ -5,73 +5,113 @@
 #include <limits.h>
 #include <stdbool.h>
 
-void echo(char **input_array, int size)
+void echo(char *input[], int size)
 {
-    int flagger4 = INT_MAX;
-    int flagger5 = INT_MAX;
-    int flagger6 = INT_MAX;
-    char echo_input[10];
-    if (input_array[1] != NULL)
+    bool echoN = false;
+    bool echoE = false;
+    bool echoHelp = false;
+
+    //Determining the type of echo
+    if(strcmp(input[1], "-n") == 0)
     {
-        strcpy(echo_input, input_array[1]);
-        flagger4 = strcmp(echo_input, "-n");
-        flagger5 = strcmp(echo_input, "-E");
-        flagger6 = strcmp(echo_input, "--help");
+        echoN = true;
     }
-    else
+    else if(strcmp(input[1], "-e") == 0)
     {
-        return;
+        echoE = true;
     }
-    if (flagger4 == 0)
+    else if(strcmp(input[1], "--help") == 0)
     {
-        nextLine = 1;
-        echon(input_array, size);
+        echoHelp = true;
     }
-    else if (flagger5 == 0)
-    {
-        echoE(input_array, size);
-    }
-    else if (flagger6 == 0)
-    {
-        printf("%s", "this command prints the input given after echo");
-    }
-    else
-    {
-        for (int i = 1; i < size; ++i)
+
+    //implmenting the echo accourding to the type
+    if(echoN){
+        for(int i = 2; i < size; i++)
         {
-            if (input_array[i] != NULL)
-            {
-                printf("%s ", input_array[i]);
-            }
+            printf("%s ", input[i]);
         }
+    }
+    else if(echoE){
+        for(int i = 2; i < size; i++)
+        {
+            printf("%s ", input[i]);
+        }
+        printf("\n");
+    }
+    else if(echoHelp){
+        printf("Usage: echo [SHORT-OPTION]... [STRING]...\n");
+        printf("Echo the STRING(s) to standard output.\n");
+        printf("  -n\tdo not output the trailing newline\n");
+        printf("  -e\tenable interpretation of backslash escapes\n");
+        printf("  --help\tdisplay this help and exit\n");
+    }
+    else{
+        for(int i = 1; i < size; i++)
+        {
+            printf("%s ", input[i]);
+        }
+        printf("\n");
     }
 }
 
-void echon(char **input_array, int size)
+void pwd(char *input[], int size)
 {
-    int i = 2;
-    while (i < size)
+    char dir[1000];
+    getcwd(dir, 1000);
+    if(input[1]==NULL){
+        printf("%s\n", dir);
+    }
+    else if(strcmp(input[1], "--help") == 0){
+        printf("Usage: pwd [OPTION]...\n");
+        printf("Print the name of the current working directory.\n");
+        printf("  --help\tdisplay this help and exit\n");
+    }
+    else if (strcmp(input[1], "-P")
     {
-        if (input_array[i] != NULL)
-        {
-            printf("%s ", input_array[i]);
-        }
-        i++;
+        char newarr[1024];
+        char *cwd2 = getcwd(newarr, sizeof(newarr));
+        printf("%s", cwd2);
+    }
+    else if (strcmp(input[1], "-L")
+    {
+        char buf[PATH_MAX];
+        char newarr[1024];
+        char *res = realpath(newarr, buf);
+        char *cwd2 = getcwd(newarr, sizeof(newarr));
+        printf("%s", cwd2);
+    }
+    else{
+        printf("pwd: invalid option -- '%s'\n", input[1]);
+        printf("Try 'pwd --help' for more information.\n");
     }
 }
 
-void echoE(char **input_array, int size)
-{
-    int i = 2;
-    while (i < size)
-    {
-        if (input_array[i] != NULL)
-        {
-            printf("%s ", input_array[i]);
-        }
-        i++;
-    }
-}
+// void echon(char **input_array, int size)
+// {
+//     int i = 2;
+//     while (i < size)
+//     {
+//         if (input_array[i] != NULL)
+//         {
+//             printf("%s ", input_array[i]);
+//         }
+//         i++;
+//     }
+// }
+
+// void echoE(char **input_array, int size)
+// {
+//     int i = 2;
+//     while (i < size)
+//     {
+//         if (input_array[i] != NULL)
+//         {
+//             printf("%s ", input_array[i]);
+//         }
+//         i++;
+//     }
+// }
 
 
 int main(){
@@ -80,38 +120,40 @@ int main(){
     while (true)
     {
         char rawCommand[100];
-        char s[100];
+        char commandCopy[100];
         printf(">> ");
         scanf("%[^\n]%*c", rawCommand);
-        char *command = strtok(rawCommand, " ");
-        if (strcmp(command, "exit") == 0)
+        strcpy(commandCopy, rawCommand);
+        
+        //parsing the input
+        int size = 0;
+        char *token = strtok(rawCommand, " ");
+        char **input = (char **)malloc(500 * sizeof(char *));
+        
+        //filling the input array
+        while (token != NULL)
+        {
+            input[size] = (char *)malloc(500 * sizeof(char));
+            strcpy(input[size], token);
+            token = strtok(NULL, " ");
+            size++;
+        }
+
+        if (strcmp(input[0], "exit") == 0)
         {
             printf("Exiting the shell\n");
             break;
         }
-        else if(strcmp(command, "pwd") == 0){
-            printf("%s\n",getcwd(s,100));
+        else if(strcmp(input[0], "pwd") == 0){
+            pwd(input, size);
         }
-        else if (strcmp(command, "echo") == 0)
+        else if (strcmp(input[0], "echo") == 0)
         {
-            while (command != NULL)
-            {   
-                if(strcmp(command, "echo") == 0){
-                    command = strtok(NULL, " ");
-                    continue;
-                }
-                if(strcmp(command, "-n") == 0){
-                    command = strtok(NULL, " ");
-                    printf("%s",command);
-                }
-                else{
-                    printf("%s\n",command);
-                }
-            }
+            echo(input,size);
         }
-        else if (strcmp(command, "cd") == 0)
+        else if (strcmp(input[0], "cd") == 0)
         {
-            char *cd = strtok(command, " ");
+            char *cd = strtok(input[0], " ");
             cd = strtok(NULL, " ");
             if (cd == NULL)
             {
