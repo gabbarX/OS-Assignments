ef5bcc5fa63c34509fd86fb51fd7d978fb96c4ce
gabbarX <ankit.rr528@gmail.com>
2022-10-26 00:47:24 +0530

    date updated

--- Assignment_1/date.c
+++ Assignment_1/date.c
@@ -2,51 +2,9 @@
 #include <time.h>
 #include <string.h>
 
-void Date()
-{
-    time_t rawtime = time(NULL);
-    char day[4], month[4], timezone[6];
-    struct tm *ptm;
-    ptm = localtime(&rawtime);
-    strftime(day, 4, "%a", ptm);
-    strftime(month, 4, "%b", ptm);
-    printf("%s %s %02d %02d:%02d:%02d %d IST\n", day, month, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec,ptm->tm_year + 1900);
-}
 
-void DateU()
-{
-    time_t rawtime = time(NULL);
-    struct tm *ptm;
-    char day[4], month[4];
-    ptm = gmtime(&rawtime);
-    strftime(day, 4, "%a", ptm);
-    strftime(month, 4, "%b", ptm);
-    printf("%s %s %02d %02d:%02d:%02d %d UTC\n", day, month, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, ptm->tm_year + 1900);
-}
-
-void DateR()
-{
-    time_t rawtime = time(NULL);
-    struct tm *ptm;
-    char day[4], month[4], timezone[6];
-    ptm = localtime(&rawtime);
-    strftime(day, 4, "%a", ptm);
-    strftime(month, 4, "%b", ptm);
-    strftime(timezone, 6, "%z", ptm);
-    printf("%s, %02d %s %d %02d:%02d:%02d %s\n", day, ptm->tm_mday, month, ptm->tm_year + 1900, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, timezone);
-}
-
-void DateHelp()
-{
-    printf("\nUsage: date [OPTION]\n");
-    printf("Display date and time in the given FORMAT.\n");
-    printf("-u \t print or set Coordinated Universal Time (UTC)\n");
-    printf("-R \t output date and time in RFC 5322 format.\n");
-    printf("--help \t brings up this help text.\n");
-}
 int main(int argc, char *argv[])
 {
-
     char command[100] = "";
     char flags[100] = "";
     char *token = strtok(argv[1], " ");
@@ -58,19 +16,42 @@ int main(int argc, char *argv[])
     }
     if (!strcmp(flags, ""))
     {
-        Date();
+        time_t rawtime = time(NULL);
+        char day[4], month[4], timezone[6];
+        struct tm *ptm;
+        ptm = localtime(&rawtime);
+        strftime(day, 4, "%a", ptm);
+        strftime(month, 4, "%b", ptm);
+        printf("%s %s %02d %02d:%02d:%02d %d IST\n", day, month, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec,ptm->tm_year + 1900);
     }
     else if (flags[0] == '-' && flags[1] == 'u')
     {
-        DateU();
+        time_t rawtime = time(NULL);
+        struct tm *ptm;
+        char day[4], month[4];
+        ptm = gmtime(&rawtime);
+        strftime(day, 4, "%a", ptm);
+        strftime(month, 4, "%b", ptm);
+        printf("%s %s %02d %02d:%02d:%02d %d UTC\n", day, month, ptm->tm_mday, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, ptm->tm_year + 1900);
     }
     else if (flags[0] == '-' && flags[1] == 'R')
     {
-        DateR();
+        time_t rawtime = time(NULL);
+        struct tm *ptm;
+        char day[4], month[4], timezone[6];
+        ptm = localtime(&rawtime);
+        strftime(day, 4, "%a", ptm);
+        strftime(month, 4, "%b", ptm);
+        strftime(timezone, 6, "%z", ptm);
+        printf("%s, %02d %s %d %02d:%02d:%02d %s\n", day, ptm->tm_mday, month, ptm->tm_year + 1900, ptm->tm_hour, ptm->tm_min, ptm->tm_sec, timezone);
     }
     else if (flags[0] == '-' && flags[1] == '-' && flags[2] == 'h' && flags[3] == 'e'
                 && flags[4] == 'l' && flags[5] == 'p'){
-        DateHelp();
+        printf("\nUsage: date [OPTION]\n");
+        printf("Display date and time in the given FORMAT.\n");
+        printf("-u \t print or set Coordinated Universal Time (UTC)\n");
+        printf("-R \t output date and time in RFC 5322 format.\n");
+        printf("--help \t brings up this help text.\n");
     }
     else
     {
