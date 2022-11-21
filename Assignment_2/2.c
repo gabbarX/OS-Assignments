508615ab24e6929daa991808c84f65407ff7c973
gabbarX <ankit.rr528@gmail.com>
2022-11-21 19:20:00 +0530

    linux kernel update

--- Assignment_2/2.c
+++ Assignment_2/2.c
@@ -1,18 +1,16 @@
 #include <linux/kernel.h>
-#include <sys/syscall.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
 
-
-
-SYSCALL_DEFINE4(kernel_2d_memcpy, int**, src, int**, dest, int, row, int, col)
+SYSCALL_DEFINE3(kernel_2d_memcpy, int*, src, int*, dest, int, len)
 {     
-
-   int buffer[5][5]; 
+    int buffer[2][2]; 
     
-   if (__copy_from_user(buffer, src, sizeof(int)*(row)*(col)))
-      return -EFAULT;
+    if (__copy_from_user(buffer, src, sizeof(int)*(len)*(2)))
+       return -EFAULT;
 
-   if (__copy_to_user(dest, buffer, sizeof(int)*(row)*(col)))
-      return -EFAULT;
+    if (__copy_to_user(dest, buffer, sizeof(int)*(len)*(2)))
+       return -EFAULT;
 
-   return 0;
+    return 0;
 }
