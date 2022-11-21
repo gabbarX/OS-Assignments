ebbc759965ab1f8ad7815cea58475bed6e2dc5ec
gabbarX <ankit.rr528@gmail.com>
2022-11-21 13:08:47 +0530

    fafaf

--- Assignment_2/2.c
+++ Assignment_2/2.c
@@ -3,15 +3,16 @@
 
 
 
-SYSCALL_DEFINE3(kernel_2d_memcpy, int*, src, int*, dest, int, len)
-{
-    int buffer[len][2]; 
+SYSCALL_DEFINE4(kernel_2d_memcpy, int**, src, int**, dest, int, row, int, col)
+{     
+
+   int buffer[5][5]; 
     
-    if (__copy_from_user(buffer, src, sizeof(int)*(len)*(2)))
-       return -EFAULT;
+   if (__copy_from_user(buffer, src, sizeof(int)*(row)*(col)))
+      return -EFAULT;
 
-    if (__copy_to_user(dest, buffer, sizeof(int)*(len)*(2)))
-       return -EFAULT;
+   if (__copy_to_user(dest, buffer, sizeof(int)*(row)*(col)))
+      return -EFAULT;
 
-    return 0;
+   return 0;
 }
--- Assignment_2/temp.c
+++ Assignment_2/temp.c
@@ -8,8 +8,6 @@ void copy(int src[][], int dest[][], int r, int c)
     int buffer[r][c];
     int count=0;
     for(int i=0;i<r;i++){
-        // int check = __copy_from_user(tempbuffer, src, sizeof(int)* (c));
-        // __copy_to_user(dest, tempbuffer, sizeof(int)* (c));
         for(int j=0;j<c;j++){
             tempbuffer[j] = src[] 
         }
