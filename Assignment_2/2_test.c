732c3cbda367b2a46f392ab20c7589097cf4f35a
gabbarX <ankit.rr528@gmail.com>
2022-11-21 14:25:23 +0530

    testupdate

--- Assignment_2/2_test.c
+++ Assignment_2/2_test.c
@@ -1,11 +1,32 @@
-#include<stdio.h>
-#include<sys/syscall.h>
-#include<linux/kernel.h>
-#include<unistd.h>
-#include<errno.h>
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <linux/kernel.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define kernel_2d_memcpy 451
 
 int main()
 {
+    long sys_call_status;
+    int arr1[3][3] = {{1,2,3},{1,2,3},{1,2,3}};
+    int testArr[3][3];
 
-    
+    sys_call_status = syscall(kernel_2d_memcpy,arr1,&testArr,3,3);
+
+    if (sys_call_status != EFAULT)
+    {
+        printf("Successfully invoked system call 451\n");
+    }
+    else{
+        perror("Error: ");
+    }
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%d ",testArr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
