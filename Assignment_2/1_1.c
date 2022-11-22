b8096406a9c38e45a1f4e437040c395966ab6319
gabbarX <ankit.rr528@gmail.com>
2022-11-22 15:39:45 +0530

    adadad

--- Assignment_2/1_1.c
+++ Assignment_2/1_1.c
@@ -6,6 +6,21 @@
 #include <errno.h>
 
 void* threadA(void *vargp){
+    struct sched_param paramA;
+    int policyA = SCHED_OTHER;
+    paramA.sched_priority = 0;
+    // int priority = 0;
+
+    int ta = pthread_setschedparam(pthread_self(), policyA, &paramA);
+    if(ta==0){
+        printf("Successfully initialised the policy and priority for thread A.\n");
+    }
+    else{
+        printf("A ; ");
+        perror("Error");
+    }
+
+
     long long int i=1;
     printf("Hello threadA reporting sir.\n");
     while (i<=pow(2,32)) 
@@ -15,15 +30,49 @@ void* threadA(void *vargp){
 }
 
 void* threadB(void *vargp){
+
+
+    struct sched_param paramB;
+    int policyB = SCHED_RR;
+    paramB.sched_priority = 1;
+    // int priority = 0;
+
+    int tb = pthread_setschedparam(pthread_self(), policyB, &paramB);
+    if(tb==0){
+        printf("Successfully initialised the policy and priority for thread B.\n");
+    }
+    else{
+        printf("B ; ");
+        perror("Error");
+    }
+        
     long long int i=1;
     printf("Hello threadB reporting sir.\n");
     while (i<=pow(2,32)) 
     {
         i++;
     }
+    // printf("Thread B complete.\n");
+    
 }
 
 void* threadC(void *vargp){
+
+    struct sched_param paramC;
+    int policyC = SCHED_FIFO;
+    paramC.sched_priority = 1;
+    // int priority = 0;
+
+    int tc = pthread_setschedparam(pthread_self(), policyC, &paramC);
+    if(tc==0){
+        printf("Successfully initialised the policy and priority for thread C.\n");
+    }
+    else{
+        printf("C ; ");
+        perror("Error");
+    }
+    
+
     long long int i=1;
     printf("Hello threadC reporting sir.\n");
     while (i<=pow(2,32)) 
@@ -38,25 +87,6 @@ int main(void)
     pthread_t threadBid;
     pthread_t threadCid;
 
-    struct sched_param paramA;
-    int policyA = SCHED_OTHER;
-    paramA.sched_priority = 0;
-    // int priority = 0;
-
-
-    struct sched_param paramB;
-    int policyB = SCHED_RR;
-    paramB.sched_priority = 1;
-    // int priority = 0;
-
-
-
-    struct sched_param paramC;
-    int policyC = SCHED_FIFO;
-    paramC.sched_priority = 1;
-    // int priority = 0;
-
-
    
     int cta = pthread_create(&threadAid, NULL, threadA, (void *)&threadAid);
     if(cta==0){
@@ -66,13 +96,6 @@ int main(void)
         perror("Error");
     }
 
-    int ta = pthread_setschedparam(threadAid, policyA, &paramA);
-    if(ta==0){
-        printf("Successfully initialised the policy and priority for thread A.\n");
-    }
-    else{
-        perror("Error");
-    }
 
 
     // clock_gettime();
@@ -89,17 +112,9 @@ int main(void)
     else{
         perror("Error");
     }
-    int tb = pthread_setschedparam(threadBid, policyB, &paramB);
-    if(tb==0){
-        printf("Successfully initialised the policy and priority for thread B.\n");
-    }
-    else{
-        perror("Error");
-    }
     
     // clock_gettime();
     
-
     int ctc = pthread_create(&threadCid, NULL, threadC, (void *)&threadCid);
     if(ctc==0){
         printf("ThreadC created successfully.\n");
@@ -107,17 +122,6 @@ int main(void)
     else{
         perror("Error");
     }
-
-
-    int tc = pthread_setschedparam(threadCid, policyC, &paramC);
-    if(tc==0){
-        printf("Successfully initialised the policy and priority for thread A.\n");
-    }
-    else{
-        perror("Error");
-    }
-    
-
 
     pthread_join(threadAid, NULL);
     pthread_join(threadBid, NULL);
