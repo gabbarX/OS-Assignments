3687252b3e78e83b45c2e1202d92af0c5cfd571f
gabbarX <ankit.rr528@gmail.com>
2022-11-22 15:39:37 +0530

    adadadad

--- Assignment_2/2.c
+++ Assignment_2/2.c
@@ -1,16 +1,18 @@
 #include <linux/kernel.h>
-#include <linux/kernel.h>
-#include <linux/syscalls.h>
+#include <sys/syscall.h>
 
-SYSCALL_DEFINE3(kernel_2d_memcpy, int*, src, int*, dest, int, len)
+
+
+SYSCALL_DEFINE4(kernel_2d_memcpy, int**, src, int**, dest, int, row, int, col)
 {     
-    int buffer[2][2]; 
+
+   int buffer[5][5]; 
     
-    if (__copy_from_user(buffer, src, sizeof(int)*(len)*(2)))
-       return -EFAULT;
+   if (__copy_from_user(buffer, src, sizeof(int)*(row)*(col)))
+      return -EFAULT;
 
-    if (__copy_to_user(dest, buffer, sizeof(int)*(len)*(2)))
-       return -EFAULT;
+   if (__copy_to_user(dest, buffer, sizeof(int)*(row)*(col)))
+      return -EFAULT;
 
-    return 0;
+   return 0;
 }
