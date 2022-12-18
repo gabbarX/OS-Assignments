454a5405d8ec0d175969890b333c0a37c0bad5bf
gabbarX <ankit.rr528@gmail.com>
2022-12-18 15:40:30 +0530

    fifo update

--- Assignment_3/question_2/p2_fifo.c
+++ Assignment_3/question_2/p2_fifo.c
@@ -6,16 +6,22 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define FIFO_NAME "fifo"
+#define FIFO_NAME "hehe"
 #define STRING_LENGTH 10
 #define GROUP_SIZE 5
 
 int main() {
-int fd, id;
-char buffer[GROUP_SIZE][STRING_LENGTH];
+    int fd, id;
+    char buffer[GROUP_SIZE][STRING_LENGTH];
 
-// Open the FIFO file for reading
-if ((fd = open(FIFO_NAME, O_RDONLY)) < 0) {
-perror("Error opening FIFO file");
-exit(1);
+    // Open the FIFO file for reading
+    if ((fd = open(FIFO_NAME, O_RDONLY)) < 0) 
+    {
+        perror("Error opening FIFO file");
+        exit(1);
+    }
+    
+
+
+
 }
