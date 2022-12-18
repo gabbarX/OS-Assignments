88eacb11128c1a411ce08624fe00868db03cd278
gabbarX <ankit.rr528@gmail.com>
2022-12-18 15:40:46 +0530

    p1_fifo update

--- Assignment_3/question_2/p1_fifo.c
+++ Assignment_3/question_2/p1_fifo.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 #define MAX_LEN 50
-#define FIFO_NAME "myfifo"
+#define FIFO_NAME "hehe"
 #define NUM_STRINGS 50
 #define GROUP_SIZE 5
 
@@ -16,14 +16,14 @@ int main()
     int fd;
     char *strings[NUM_STRINGS];
     char buf[MAX_LEN + 1];
-    int i, j;
+    // int i, j;
     int acknowledged_id = -1;
 
     // Generate the random strings and store them in the 'strings' array
-    for (i = 0; i < NUM_STRINGS; i++)
+    for (int i = 0; i < NUM_STRINGS; i++)
     {
         strings[i] = malloc(MAX_LEN + 1);
-        for (j = 0; j < MAX_LEN; j++)
+        for (int j = 0; j < MAX_LEN; j++)
         {
             strings[i][j] = 'a' + (rand() % 26);
         }
@@ -49,13 +49,13 @@ int main()
     }
 
     // Send the strings in groups of 5 to P2
-    i = 0;
-    while (i < NUM_STRINGS)
+    int k = 0;
+    while (k < NUM_STRINGS)
     {
         // Send the strings
-        for (j = 0; j < GROUP_SIZE && i < NUM_STRINGS; j++, i++)
+        for (j = 0; j < GROUP_SIZE && k < NUM_STRINGS; j++, k++)
         {
-            sprintf(buf, "%d:%s", i, strings[i]);
+            sprintf(buf, "%d:%s", k, strings[k]);
             write(fd, buf, strlen(buf));
         }
 
