bc72478e69833867046b3c8370d3fd76732910b7
gabbarX <ankit.rr528@gmail.com>
2022-12-18 12:41:36 +0530

    question1 partA complete but unverified

--- Assignment_3/q1.c
+++ Assignment_3/q1.c
@@ -1,5 +1,3 @@
-Here is a program in C to simulate the philosophers using threads and the forks using global variables, and resolve the deadlock using strict ordering of resource requests:
-
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,53 +8,65 @@ Here is a program in C to simulate the philosophers using threads and the forks 
 
 int forks[NUM_FORKS];
 
-void *philosopher(void *arg) {
-int id = *((int *)arg);
+void *philosopher(void *arg) 
+{
+    int id = *((int *)arg);
 
-while (1) {
-// Think
-printf("Philosopher %d is thinking.\n", id);
-sleep(1);
+    while (1) 
+    {
+        // Think
+        printf("Philosopher %d is thinking.\n", id);
+        sleep(1);
+        // Pick up left fork
+        printf("Philosopher %d is trying to pick up left fork.\n", id);
+    
+        while (forks[id] == 1) 
+        {
+        // Do nothing
+        }
+        
+        forks[id] = 1;
+        printf("Philosopher %d picked up left fork.\n", id);
 
-Copy code
-// Pick up left fork
-printf("Philosopher %d is trying to pick up left fork.\n", id);
-while (forks[id] == 1) {
-  // Do nothing
-}
-forks[id] = 1;
-printf("Philosopher %d picked up left fork.\n", id);
+        // Pick up right fork
+        printf("Philosopher %d is trying to pick up right fork.\n", id);
+        
+        while (forks[(id + 1) % NUM_FORKS] == 1) 
+        {
+        // Do nothing
+        }
+        forks[(id + 1) % NUM_FORKS] = 1;
+        printf("Philosopher %d picked up right fork.\n", id);
 
-// Pick up right fork
-printf("Philosopher %d is trying to pick up right fork.\n", id);
-while (forks[(id + 1) % NUM_FORKS] == 1) {
-  // Do nothing
-}
-forks[(id + 1) % NUM_FORKS] = 1;
-printf("Philosopher %d picked up right fork.\n", id);
+        // Eat
+        printf("Philosopher %d is eating.\n", id);
+        sleep(1);
 
-// Eat
-printf("Philosopher %d is eating.\n", id);
-sleep(1);
-
-// Put down forks
-forks[id] = 0;
-forks[(id + 1) % NUM_FORKS] = 0;
-}
-}
-
-int main() {
-pthread_t philosophers[NUM_PHILOSOPHERS];
-int ids[NUM_PHILOSOPHERS];
-
-for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-ids[i] = i;
-pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
+        // Put down forks
+        forks[id] = 0;
+        forks[(id + 1) % NUM_FORKS] = 0;
+    }
 }
 
-for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
-pthread_join(philosophers[i], NULL);
-}
+int main() 
+{
+    pthread_t philosophers[NUM_PHILOSOPHERS];
+    int ids[NUM_PHILOSOPHERS];
 
-return 0;
+    //creating philosopher threads
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
+    {
+        ids[i] = i;
+        pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
+    }
+
+    //Joining philosopher threads
+    for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
+    {
+        pthread_join(philosophers[i], NULL);
+    }
+
+    printf("All philoshophers have completed their tasks!");
+
+    return 0;
 }
