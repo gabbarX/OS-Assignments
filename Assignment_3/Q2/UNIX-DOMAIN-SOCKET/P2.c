884d1c210ce22a84b464c65f6dd535964820b62a
gabbarX <ankit.rr528@gmail.com>
2022-12-20 02:59:23 +0530

    rawcodeupdate

--- Assignment_3/Q2/UNIX-DOMAIN-SOCKET/P2.c
+++ Assignment_3/Q2/UNIX-DOMAIN-SOCKET/P2.c
@@ -13,14 +13,13 @@
 
 int main()
 {
-    // create unix domain socket
+
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("Error creating socket");
         exit(1);
     }
 
-    // bind socket to name
     struct sockaddr_un addr;
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
@@ -30,55 +29,48 @@ int main()
         exit(1);
     }
 
-    // listen for incoming connections
     if (listen(sockfd, 5) < 0) {
         perror("Error listening for incoming connections");
         exit(1);
     }
 
-    // accept connection from P1
     int clientfd = accept(sockfd, NULL, NULL);
     if (clientfd < 0) {
         perror("Error accepting connection");
         exit(1);
     }
 
-    // receive strings and IDs from P1
     while (1) {
-        int highest_id = -1;  // highest ID received
+        int highest_id = -1; 
 
-        for (int i = 0; i < NUM_STRINGS; i++) {
-            // receive ID
+        for (int i = 0; i < NUM_STRINGS; i++) 
+        {
             int id;
             if (read(clientfd, &id, sizeof(id)) < 0) {
                 perror("Error receiving ID");
                 exit(1);
             }
 
-            // receive string
+
             char str[MAX_STR_LEN];
             if (read(clientfd, str, MAX_STR_LEN) < 0) {
                 perror("Error receiving string");
                 exit(1);
             }
 
-            // print ID and string
             printf("ID: %d, String: %s\n", id, str);
 
-            // update highest ID
             if (id > highest_id) {
                 highest_id = id;
             }
         }
 
-        // send highest ID back to P1
         if (write(clientfd, &highest_id, sizeof(highest_id)) < 0) {
             perror("Error sending highest ID");
             exit(1);
         }
     }
 
-    // close socket
     close(clientfd);
     close(sockfd);
 
