c4bf7e76a2143d9749e542c70fb416e9f679f31e
gabbarX <ankit.rr528@gmail.com>
2022-12-20 00:21:01 +0530

    shared mem not done

--- Assignment_3/Q2/Shared-Memory/p1.c
+++ Assignment_3/Q2/Shared-Memory/p1.c
@@ -18,41 +18,54 @@ int main()
     int acknowledged_id = -1;
     int group = 5;
     char buf[maxlen];
-
-    char strings[stringNum][maxlen];
-    for (int i = 0; i < stringNum; i++) {
-        for (int j = 0; j < maxlen; j++) {
-            strings[i][j] = 'a' + rand() % 26;
+    char *strings[stringNum];
+    for (int i = 0; i < stringNum; i++)
+    {
+        strings[i] = malloc(maxlen + 1);
+        for (int j = 0; j < maxlen; j++)
+        {
+        strings[i][j] = 'a' + (rand() % 26);
         }
-        strings[i][maxlen]='\0';
+        strings[i][maxlen] = '\0';
     }
 
+    int size = sizeof(strings);
     printf("Creating shared memory segment!\n");
     int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
     if (fd == -1) {
         perror("shm_open");
-        return 1;
+        return EXIT_FAILURE;
     }
     else{
         printf("Shared Memory initialised successfully!\n");
     }
-
-    int k = 0;
-    while (k < stringNum)
+    ftruncate(fd,size);
+    char* ptr = (char*)mmap(0,size,PROT_WRITE,MAP_SHARED,fd,0);
+    for(int i=0;i<stringNum;i++)
     {
-        for (int j = 0; j < group && k < stringNum; j++, k++)
+        for(int j=0;j<50;j++)
         {
-            sprintf(buf, "%s",strings[k]);
-            write(fd, buf, strlen(buf));
+            ptr[i] = strings[i];
         }
-
-        int num_read = read(fd, buf, maxlen);
-        buf[num_read] = '\0';
-        sscanf(buf, "%d", &acknowledged_id);
-
     }
+
+    // int k = 0;
+    // while (k < stringNum)
+    // {
+    //     for (int j = 0; j < group && k < stringNum; j++, k++)
+    //     {
+    //         sprintf(buf, "%s",strings[k]);
+    //         write(ptr, buf, strlen(buf));
+    //     }
+
+    //     int num_read = read(ptr, buf, maxlen);
+    //     buf[num_read] = '\0';
+    //     sscanf(buf, "%d", &acknowledged_id);
+
+    // }
     
     close(fd);
+    shm_unlink(SHM_NAME);
 
 
     return 0;
--- Assignment_3/Q2/Shared-Memory/p2.c
+++ Assignment_3/Q2/Shared-Memory/p2.c
@@ -16,32 +16,29 @@ int main()
     int STR_LEN = 5;
     int NUM_STRINGS = 50;
     int GROUP_SIZE = 5;
-    // sleep(5);
-    // key_t key = ftok(SHM_NAME,65);
-    // int shmid = shmget(key,1024,0666|IPC_CREAT);
-    // char *str = (char*) shmat(shmid,(void*)0,0);
     int fd = shm_open(SHM_NAME, O_RDONLY, 0666);
 
 
     int highest_id = -1;
     while (true) 
     {
-    char buffer[STR_LEN];
-    int id;
-    while (read(fd, buffer, STR_LEN) > 0) 
-    {
-        sscanf(buffer, "%d %s", &id, buffer);
-        printf("Received string with ID %d: %s\n", id, buffer);
-        if (id > highest_id) 
+        char buffer[STR_LEN];
+        int id;
+        while (read(fd, buffer, STR_LEN) > 0) 
         {
-        highest_id = id;
+            sscanf(buffer, "%d %s", &id, buffer);
+            printf("Received string with ID %d: %s\n", id, buffer);
+            if (id > highest_id) 
+            {
+                highest_id = id;    
+            }
+            id++;
         }
-        id++;
-    }
-        // printf("Highest ID: %d\n",highest_id);
+
     }
 
     // shmdt(str);
     close(fd);
+    shm_unlink(SHM_NAME);
     return 0;
 }
