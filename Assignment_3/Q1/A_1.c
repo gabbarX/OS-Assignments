83bf227509f35c3c7c3d5df5744acf7ec6056e90
gabbarX <ankit.rr528@gmail.com>
2022-12-20 04:30:47 +0530

    question1 partA update

--- Assignment_3/Q1/A_1.c
+++ Assignment_3/Q1/A_1.c
@@ -2,72 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
 
-#define NUM_PHILOSOPHERS 5
-#define NUM_FORKS 5
+int PhilosopherNum=5;
+int f = 5;
+int forks[5];
 
-int forks[NUM_FORKS];
-
-void *philosopher(void *arg) 
+void *philosophersWorld(void *arg) 
 {
     int id = *((int *)arg);
 
-    while (1) 
+    while (true) 
     {
-        // Think
         printf("Philosopher %d is thinking.\n", id);
         sleep(1);
-        // Pick up left fork
-        printf("Philosopher %d is trying to pick up left fork.\n", id);
-    
-        while (forks[id] == 1) 
-        {
-        // Do nothing
-        }
         
         forks[id] = 1;
         printf("Philosopher %d picked up left fork.\n", id);
-
-        // Pick up right fork
-        printf("Philosopher %d is trying to pick up right fork.\n", id);
         
-        while (forks[(id + 1) % NUM_FORKS] == 1) 
-        {
-        // Do nothing
-        }
-        
-        forks[(id + 1) % NUM_FORKS] = 1;
+        forks[(id + 1) % f] = 1;
         printf("Philosopher %d picked up right fork.\n", id);
 
-        // Eat
         printf("Philosopher %d is eating.\n", id);
         sleep(1);
 
-        // Put down forks
         forks[id] = 0;
-        forks[(id + 1) % NUM_FORKS] = 0;
+        forks[(id + 1) % f] = 0;
     }
 }
 
 int main() 
 {
-    pthread_t philosophers[NUM_PHILOSOPHERS];
-    int ids[NUM_PHILOSOPHERS];
+    pthread_t philosophers[PhilosopherNum];
+    int ids[PhilosopherNum];
 
-    //creating philosopher threads
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
+    for (int i = 0; i < PhilosopherNum; i++) 
     {
         ids[i] = i;
-        pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
+        pthread_create(&philosophers[i], NULL, philosophersWorld, &ids[i]);
     }
 
-    //Joining philosopher threads
-    for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
+    for (int i = 0; i < PhilosopherNum; i++) 
     {
         pthread_join(philosophers[i], NULL);
     }
-
-    printf("All philoshophers have completed their tasks!");
 
     return 0;
 }
--- Assignment_3/Q1/A_2.c
+++ Assignment_3/Q1/A_2.c
@@ -1,7 +1,8 @@
-//Modified Dining philosophers Problem using semaphores
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 #define NUM_PHILOSOPHERS 5
 sem_t forks[NUM_PHILOSOPHERS];
@@ -10,29 +11,23 @@ void *philosopher(void *arg)
 {
     int id = *((int *)arg);
 
-    while (1) 
+    while (true) 
     {
         printf("Philosopher %d is thinking.\n", id);
-        //Thinking
         sleep(2);
 
-        // pick up left fork
         sem_wait(&forks[id]);
         printf("Philosopher %d picked up left fork.\n", id);
 
-        // pick up right fork
         sem_wait(&forks[(id + 1) % NUM_PHILOSOPHERS]);
         printf("Philosopher %d picked up right fork.\n", id);
 
         printf("Philosopher %d is eating.\n", id);
-        //Thinking
         sleep(2);
 
-        // put down left fork
         sem_post(&forks[id]);
         printf("Philosopher %d put down left fork.\n", id);
 
-        // put down right fork
         sem_post(&forks[(id + 1) % NUM_PHILOSOPHERS]);
         printf("Philosopher %d put down right fork.\n", id);
 
@@ -45,20 +40,17 @@ int main(void)
     pthread_t philosophers[NUM_PHILOSOPHERS];
     int ids[NUM_PHILOSOPHERS];
 
-    // initialize semaphores
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
     {
         sem_init(&forks[i], 0, 1);
     }
 
-    // create threads for each philosopher
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
     {
         ids[i] = i;
         pthread_create(&philosophers[i], NULL, philosopher, &ids[i]);
     }
 
-    // wait for threads to finish
     for (int i = 0; i < NUM_PHILOSOPHERS; i++) 
     {
         pthread_join(philosophers[i], NULL);
