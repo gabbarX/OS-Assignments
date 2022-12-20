896ddc5db4cc175c343f9b345ccb7c3602e73131
gabbarX <ankit.rr528@gmail.com>
2022-12-20 21:55:57 +0530

    code update

--- Assignment_3/Q3/syscall.c
+++ Assignment_3/Q3/syscall.c
@@ -19,3 +19,23 @@ static void __exit hello_cleanup(void)
 }
 module_init(hello_init);      
 module_exit(hello_cleanup);   
+
+//Define syscall
+#define PROCESS_INFO 287
+
+//Define the system call
+SYSCALL_DEFINE1(PROCESS_INFO,int,pid)
+{
+    struct task_struct *task;
+    struct pid *pid_struct;
+
+    pid_struct = find_get_pid(pid);
+    task = pid_task(pid_struct, PIDTYPE_PID);
+
+    printk("pid: %d\n",task->pid);
+    printk("user_id: %d\n",task->cred->uid.val);
+    printk("pgid: %d\n",task->group_leader->pid);
+    printk("common path: %s\n",task->comm);
+
+    return 0;
+}
