e641ec1ce3c093be3bd8e95cd8f7917377bd7a32
gabbarX <ankit.rr528@gmail.com>
2022-12-21 02:52:26 +0530

    syscall under progress

--- Assignment_3/Q3/syscall.c
+++ Assignment_3/Q3/syscall.c
@@ -9,6 +9,8 @@
 #include <linux/resource.h>
 #include <linux/blkdev.h>
 #include <linux/task_io_accounting_ops.h>
+// #include <linux/syscalls.h>
+#include <linux/unistd.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Manan Chugh");
@@ -16,15 +18,15 @@ MODULE_DESCRIPTION("Manan's custom kernel module");
 
 SYSCALL_DEFINE1(print_task_struct, pid_t, pid)
 {
-    struct task_struct task;
+    struct task_struct *task;
 
     task = pid_task(find_vpid(pid), PIDTYPE_PID);
     if (!task)
         return -EINVAL;
 
     printk(KERN_INFO "pid: %d\n", task->pid);
-    printk(KERN_INFO "user id: %d\n", task->uid);
-    printk(KERN_INFO "process group id: %d\n", task->pgid);
+    printk(KERN_INFO "user id: %d\n", __kuid_val(task->cred->uid));
+    printk(KERN_INFO "process group id: %d\n", pid_vnr(task_pgrp(task)));
     printk(KERN_INFO "command path: %s\n", task->comm);
 
     return 0;
@@ -34,7 +36,7 @@ static int __init sys_call_init(void)
 {
     long error;
 
-    error = register_syscall("print_task_struct", (void)print_task_struct);
+    error = register_syscall("print_task_struct", (void*)print_task_struct);
     if (error) {
         printk(KERN_ERR "Failed to register syscall\n");
         return error;
@@ -46,7 +48,7 @@ static int __init sys_call_init(void)
 
 static void __exit sys_call_exit(void)
 {
-    unregister_syscall("print_task_struct");
+    // unregister_sysctl_table("print_task_struct");
     printk(KERN_INFO "Syscall unregistered\n");
 }
 
