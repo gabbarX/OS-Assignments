f79965e0978ad8729cad26fe1d7a8723ba801d4a
Ankit Gautam <ankit.rr528@gmail.com>
2022-12-20 18:46:49 +0530

    Update p1.c

--- Assignment_3/Q2/Shared-Memory/p1.c
+++ Assignment_3/Q2/Shared-Memory/p1.c
@@ -9,16 +9,8 @@ char randomArr[50][12];
 void createRandomStr()
 {
 
-    int maxlen = 5;
-    int stringNum = 50;
-    int groupSize = 5;
-    int acknowledged_id = -1;
-    int group = 5;
-    char buf[maxlen];
-    char *strings[stringNum];
-
-    //generating random strings
-    for (int i = 0; i < stringNum; i++)
+    char alpha[26];
+    for (int i = 0; i < 26; i++)
     {
         alpha[i] = i % 26 + 65;
     }
@@ -29,16 +21,62 @@ void createRandomStr()
             randomArr[i][j] = alpha[rand() % 26];
         }
     }
+    for (int i = 0; i < 50; i++)
+    {
+        int c = i;
+        char c1[2];
+        c1[0] = 0;
+        sprintf(c1, "%d", c);
+        strcat(randomArr[i], c1);
+        printf("%s\n", randomArr[i]);
+    }
+}
+char buffer[12];
+int main()
+{
+    createRandomStr();
+    int i;
+    void *shared_memory;
+    char buff[100];
+    int shmid;
+    shmid = shmget((key_t)7675, sizeof(char) * 12 * 5, 0666 | IPC_CREAT);
+    // creates shared memory segment with key 2345, having size 1024 bytes. IPC_CREAT is used to create the shared segment if it does not exist. 0666 are the permissions on the shared segment
+    printf("Key of shared memory is %d\n", shmid);
+    shared_memory = shmat(shmid, NULL, 0);
+    // process attached to shared memory segment
+    printf("Process attached at %p\n", shared_memory);
+    // this prints the address where the segment is attached with this process
+    int cur = 0;
+    int Arrived = 0;
 
+    while (1)
+    {
 
-    key_t key = ftok("shmfile",65);
-    int shmid = shmget(key, 1024, 0666| IPC_CREAT);
-    char *str = (char*) shmat(shmid,(void*)0,0);
-    
-    printf("Wrtie Data: ");
-    puts(str);
+        for (int i = cur; i < cur + 5; i++)
+        {
+            strcpy(shared_memory + i * 12, randomArr[i]);
+            printf("You wrote : %s\n", (char *)shared_memory + i * 12);
+        }
+        // int condi = 0;
 
-    shmdt(str);
+        int shmid2;
+        void *shared_memory2;
+        shmid2 = shmget((key_t)1110, 1024, 0666);
+        printf("Key of 2nd shared memory is %d\n", shmid2);
+        char *t;
+        shared_memory2 = shmat(shmid, NULL, 0);
+        strcpy((char *)shared_memory2, buffer);
+        char rev[12];
+        Arrived = strtol(buffer, &t, 10);
+        printf("Data returned from shared memory is : %s\n", (char *)shared_memory2);
 
-    return 0;
+        if (cur > 49)
+        {
+            break;
+        }
+        // condi = 1;
+
+        cur = Arrived + 1;
+    }
+    // data written to shared memory
 }
