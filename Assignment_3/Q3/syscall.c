95ed854a89f704a080c07bfbabb18eabbb71d4fa
gabbarX <ankit.rr528@gmail.com>
2022-12-20 21:24:15 +0530

    syscall raw

--- Assignment_3/Q3/syscall.c
+++ Assignment_3/Q3/syscall.c
@@ -1,18 +1,17 @@
-#include <linux/module.h>     // included for all kernel modules
-#include <linux/kernel.h>     // included for KERN_INFO
-#include <linux/init.h>       // included for __init and __exit macros
+#include <linux/module.h> 
+#include <linux/kernel.h>   
+#include <linux/init.h>     
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("CSE-231-TAS");
-MODULE_DESCRIPTION("HelloWorld Linux Kernel Module."); 
+MODULE_AUTHOR("Ankit Gautam");
+MODULE_DESCRIPTION("Process Info Linux Kernel Module."); 
+
 
 static int __init hello_init(void)
 {
     printk(KERN_INFO "Hello world!\n");
     return 0;   
 }
-
-
 
 static void __exit hello_cleanup(void)
 {
